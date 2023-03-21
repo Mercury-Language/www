e93450279e8d1b1b4edf56bd4b1f957fdce9e550
Julien Fischer <juliensf@gmail.com>
2023-03-21 20:17:49 +1100

    Separate tutorials out into a separate subpage.
    include/menubar.inc:
         Add "learning" to the side menu under "Documentation.
    
    documentation/include/documentation.inc:
         Delete the tutorials from here.
    
    documentation/include/learning.inc:
    documentation/learning.php:
         Add a new subpage containing the tutorials.

---
+++ documentation/learning.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $menu="Documentation";
+    $title="Learning";
+    $dir="documentation";
+    $root="..";
+    $include="learning.inc";
+    include "$root/include/template.inc"
+?>
+</html>
